115f2c35bb634f31b68d5ffdd2e66ef6285a1c71
juan olarte <juan.olarte@utp.edu.co>
2022-06-01 14:29:22 -0500

    actualizar imagenes

--- Novatus/urls.py
+++ Novatus/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('app.urls')),
     path('api/',include('apiNovatus.urls'))
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
